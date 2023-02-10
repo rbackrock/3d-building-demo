5cf6a90e98d44441db202a636f245df896dd5cb1
yhuan <cokekokotea@gmail.com>
2023-02-10 17:13:42 +0800

    # update

--- src/views/home/components/Visualization/ThreeDimensional/World/Controls/Machine.js
+++ src/views/home/components/Visualization/ThreeDimensional/World/Controls/Machine.js
@@ -1,6 +1,15 @@
+import * as THREE from 'three'
+import {
+  CSS2DObject
+} from 'three/examples/jsm/renderers/CSS2DRenderer'
+import gsap from 'gsap'
+
 import {
   importMeshLastName
 } from '../../Utils'
+import {
+  cameraLayers
+} from '../../Camera'
 
 export default class Machine {
   constructor(mesh) {
@@ -26,29 +35,123 @@ export default class Machine {
         }
       }
     })
+
+    // animation
+    this.shellMeshDisassembleAnimation = null
+    this.baseMeshDisassembleAnimation = null
+    this.shellMeshCombineAnimation = null
+    this.baseMeshCombineAnimation = null
+
+    // 设置标签
+    this.setLabel()
+
+    // 是否已经展开
+    this.isDisassemble = false
+  }
+
+  setLabel() {
+    const coreLabelElement = document.querySelector('#machine-core-label-hook')
+    const coreHtmlObject = new CSS2DObject(coreLabelElement)
+    coreHtmlObject.layers.set(cameraLayers.DISASSEMBLE)
+    this.machine.core.mesh.add(coreHtmlObject)
+    
+    const shellLabelElement = document.querySelector('#machine-shell-label-hook')
+    const shellHtmlObject = new CSS2DObject(shellLabelElement)
+    shellHtmlObject.layers.set(cameraLayers.DISASSEMBLE)
+    this.machine.shell.mesh.add(shellHtmlObject)
+
+    const baseLabelElement = document.querySelector('#machine-base-label-hook')
+    const baseHtmlObject = new CSS2DObject(baseLabelElement)
+    baseHtmlObject.layers.set(cameraLayers.DISASSEMBLE)
+    this.machine.base.mesh.add(baseHtmlObject)
   }
 
   disassemble() {
-    const {
-      core,
-      shell,
-      base
-    } = this.machine
+    if (this.isDisassemble === false) {
+      const {
+        shell,
+        base
+      } = this.machine
+      const shellEndPosition = new THREE.Vector3(shell.mesh.position.x, shell.mesh.position.y + 3, shell.mesh.position.z)
+      const baseEndPosition = new THREE.Vector3(base.mesh.position.x, base.mesh.position.y - 2, base.mesh.position.z)
+  
+      this.shellMeshDisassembleAnimation = gsap.to(shell.mesh.position, {
+        x: shellEndPosition.x,
+        y: shellEndPosition.y,
+        z: shellEndPosition.z,
+        duration: 0.6,
+        onComplete() {
+          document.querySelector('#machine-core-label-hook').classList.add('visible')
+          document.querySelector('#machine-shell-label-hook').classList.add('visible')
+        }
+      })
+  
+      this.baseMeshDisassembleAnimation = gsap.to(base.mesh.position, {
+        x: baseEndPosition.x,
+        y: baseEndPosition.y,
+        z: baseEndPosition.z,
+        duration: 0.6,
+        onComplete() {
+          document.querySelector('#machine-base-label-hook').classList.add('visible')
+        }
+      })
 
-    core.mesh.position.set(core.mesh.position.x, core.mesh.position.y, core.mesh.position.z)
-    shell.mesh.position.set(shell.mesh.position.x, shell.mesh.position.y + 2, shell.mesh.position.z)
-    base.mesh.position.set(base.mesh.position.x, base.mesh.position.y - 2, base.mesh.position.z)
+      this.isDisassemble = true
+    }
   }
 
   combine() {
     const {
-      core,
       shell,
       base
     } = this.machine
 
-    core.mesh.position.copy(core.originPosition)
-    shell.mesh.position.copy(shell.originPosition)
-    base.mesh.position.copy(base.originPosition)
+    this.shellMeshCombineAnimation = gsap.to(shell.mesh.position, {
+      x: shell.originPosition.x,
+      y: shell.originPosition.y,
+      z: shell.originPosition.z,
+      duration: 0.6,
+      onComplete() {
+        document.querySelector('#machine-core-label-hook').classList.remove('visible')
+        document.querySelector('#machine-shell-label-hook').classList.remove('visible')
+      }
+    })
+
+    this.baseMeshCombineAnimation = gsap.to(base.mesh.position, {
+      x: base.originPosition.x,
+      y: base.originPosition.y,
+      z: base.originPosition.z,
+      duration: 0.6,
+      onComplete() {
+        document.querySelector('#machine-base-label-hook').classList.remove('visible')
+      }
+    })
+
+    this.isDisassemble = false
+  }
+
+  destroy() {
+    if (this.shellMeshDisassembleAnimation) {
+      this.shellMeshDisassembleAnimation.kill()
+      this.shellMeshDisassembleAnimation = null
+    }
+
+    if (this.baseMeshDisassembleAnimation) {
+      this.baseMeshDisassembleAnimation.kill()
+      this.baseMeshDisassembleAnimation = null
+    }
+
+    if (this.shellMeshCombineAnimation) {
+      this.shellMeshCombineAnimation.kill()
+      this.shellMeshCombineAnimation = null
+    }
+
+    if (this.baseMeshCombineAnimation) {
+      this.baseMeshCombineAnimation.kill()
+      this.baseMeshCombineAnimation = null
+    }
+
+    document.querySelector('#machine-shell-label-hook').classList.remove('visible')
+    document.querySelector('#machine-base-label-hook').classList.remove('visible')
   }
 }
--- src/views/home/components/Visualization/index.vue
+++ src/views/home/components/Visualization/index.vue
@@ -239,6 +239,64 @@ function handlerChangeDisassemble(isDisassemble) {
 
     <!-- loading -->
     <div class="loading-3d" :class="{ close: !isLoading }" id="loading-3d">资源加载中...</div>
+
+    <!-- machine-label -->
+    <div id="machine-core-label-hook" class="machine-label-container">
+      <div class="three-label-container-wrapper">
+        <div class="title-container">
+          <div class="title-container-wrapper">
+            <div class="room-id"># 机器</div>
+            <div class="room-name">核心</div>
+          </div>
+        </div>
+        <div class="icon-container">
+          <div class="icon-container-wrapper">
+            <div class="fan-container">
+              <div class="fan-part1"></div>
+              <div class="fan-part2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="machine-shell-label-hook" class="machine-label-container">
+      <div class="three-label-container-wrapper">
+        <div class="title-container">
+          <div class="title-container-wrapper">
+            <div class="room-id"># 机器</div>
+            <div class="room-name">外壳</div>
+          </div>
+        </div>
+        <div class="icon-container">
+          <div class="icon-container-wrapper">
+            <div class="fan-container">
+              <div class="fan-part1"></div>
+              <div class="fan-part2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="machine-base-label-hook" class="machine-label-container">
+      <div class="three-label-container-wrapper">
+        <div class="title-container">
+          <div class="title-container-wrapper">
+            <div class="room-id"># 机器</div>
+            <div class="room-name">基座</div>
+          </div>
+        </div>
+        <div class="icon-container">
+          <div class="icon-container-wrapper">
+            <div class="fan-container">
+              <div class="fan-part1"></div>
+              <div class="fan-part2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -252,6 +310,16 @@ function handlerChangeDisassemble(isDisassemble) {
 @media (min-width: 1900px) {
   .main-container {
     font-size: 16px
+  }
+}
+
+@keyframes fan-rotate {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
   }
 }
 
@@ -478,6 +546,93 @@ function handlerChangeDisassemble(isDisassemble) {
               }
             }
           }
+        }
+      }
+    }
+  }
+}
+
+.machine-label-container {
+  width: 200px;
+  // height: 100%;
+  color: #00deff;
+  display: none;
+
+  &.visible {
+    display: block;
+  }
+
+  .three-label-container-wrapper {
+    width: 100%;
+    height: 100%;
+
+    .title-container {
+      background-image: url('/3d/images/label/text-box.svg#svgView(preserveAspectRatio(none))');
+      background-repeat: no-repeat;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: 100% 100%;
+
+      .title-container-wrapper {
+        text-align: center;
+        pointer-events: all;
+        cursor: pointer;
+
+        .room-id {
+          padding: 2% 0 0 0;
+          font-size: 1.4em;
+        }
+
+        .room-name {
+          font-size: 1.2em;
+          padding: 2% 0 5% 0;
+        }
+      }
+    }
+
+    .icon-container {
+      .icon-container-wrapper {
+        position: relative;
+
+        .fan-container {
+          .fan-part1 {
+            width: 60px;
+            height: 60px;
+            margin: 0 auto;
+            background-image: url('/3d/images/label/fan-part1.svg#svgView(preserveAspectRatio(none))');
+            background-repeat: no-repeat;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 100% 100%;
+            animation: fan-rotate 1.3s infinite;
+            animation-fill-mode: forwards;
+            animation-timing-function: linear;
+          }
+        }
+
+        .cctv-container {
+          position: absolute;
+          top: 0;
+          right: 0;
+          width: 60px;
+          height: 60px;
+          display: flex;
+          align-items: center;
+          justify-content: flex-start;
+
+          .cctv-wrapper {
+            width: 40px;
+            height: 40px;
+            // margin: 0 auto;
+            background-image: url('/3d/images/label/cctv.svg#svgView(preserveAspectRatio(none))');
+            background-repeat: no-repeat;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 100% 100%;
+            pointer-events: all;
+            cursor: pointer;
+          }
+          
         }
       }
     }
