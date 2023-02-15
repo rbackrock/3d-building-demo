6d8b71b4dc5680bc93df8eb6c6cb26605b5e9f72
yhuan <cokekokotea@gmail.com>
2023-02-15 09:59:15 +0800

    # update

--- src/views/home/components/Visualization/index.vue
+++ src/views/home/components/Visualization/index.vue
@@ -11,6 +11,7 @@ import GroundFloorViewStatistics from './components/GroundFloorViewStatistics/in
 import SecondFloorViewStatistics from './components/SecondFloorViewStatistics/index.vue'
 import ThirdFloorViewStatistics from './components/ThirdFloorViewStatistics/index.vue'
 import FourFloorViewStatistics from './components/FourFloorViewStatistics/index.vue'
+import Machinelabel from './componentsForCssRender/Machinelabel/index.vue'
 
 import load from './ThreeDimensional/resources/index'
 import ThreeDimensional from './ThreeDimensional'
@@ -193,62 +194,7 @@ function handlerChangeDisassemble(isDisassemble) {
     <div class="loading-3d" :class="{ close: !isLoading }" id="loading-3d">资源加载中...</div>
 
     <!-- machine-label -->
-    <div id="machine-core-label-hook" class="machine-label-container">
-      <div class="three-label-container-wrapper">
-        <div class="title-container">
-          <div class="title-container-wrapper">
-            <div class="room-id"># 机器</div>
-            <div class="room-name">核心</div>
-          </div>
-        </div>
-        <div class="icon-container">
-          <div class="icon-container-wrapper">
-            <div class="fan-container">
-              <div class="fan-part1"></div>
-              <div class="fan-part2"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="machine-shell-label-hook" class="machine-label-container">
-      <div class="three-label-container-wrapper">
-        <div class="title-container">
-          <div class="title-container-wrapper">
-            <div class="room-id"># 机器</div>
-            <div class="room-name">外壳</div>
-          </div>
-        </div>
-        <div class="icon-container">
-          <div class="icon-container-wrapper">
-            <div class="fan-container">
-              <div class="fan-part1"></div>
-              <div class="fan-part2"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="machine-base-label-hook" class="machine-label-container">
-      <div class="three-label-container-wrapper">
-        <div class="title-container">
-          <div class="title-container-wrapper">
-            <div class="room-id"># 机器</div>
-            <div class="room-name">基座</div>
-          </div>
-        </div>
-        <div class="icon-container">
-          <div class="icon-container-wrapper">
-            <div class="fan-container">
-              <div class="fan-part1"></div>
-              <div class="fan-part2"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Machinelabel />
   </div>
 </template>
 
@@ -437,94 +383,6 @@ function handlerChangeDisassemble(isDisassemble) {
               }
             }
           }
-        }
-      }
-    }
-  }
-}
-
-.machine-label-container {
-  width: 200px;
-  // height: 100%;
-  color: #00deff;
-  display: none;
-  cursor: pointer;
-
-  &.visible {
-    display: block;
-  }
-
-  .three-label-container-wrapper {
-    width: 100%;
-    height: 100%;
-
-    .title-container {
-      background-image: url('/3d/images/label/text-box.svg#svgView(preserveAspectRatio(none))');
-      background-repeat: no-repeat;
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: 100% 100%;
-
-      .title-container-wrapper {
-        text-align: center;
-        pointer-events: all;
-        cursor: pointer;
-
-        .room-id {
-          padding: 2% 0 0 0;
-          font-size: 1.4em;
-        }
-
-        .room-name {
-          font-size: 1.2em;
-          padding: 2% 0 5% 0;
-        }
-      }
-    }
-
-    .icon-container {
-      .icon-container-wrapper {
-        position: relative;
-
-        .fan-container {
-          .fan-part1 {
-            width: 60px;
-            height: 60px;
-            margin: 0 auto;
-            background-image: url('/3d/images/label/fan-part1.svg#svgView(preserveAspectRatio(none))');
-            background-repeat: no-repeat;
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: 100% 100%;
-            animation: fan-rotate 1.3s infinite;
-            animation-fill-mode: forwards;
-            animation-timing-function: linear;
-          }
-        }
-
-        .cctv-container {
-          position: absolute;
-          top: 0;
-          right: 0;
-          width: 60px;
-          height: 60px;
-          display: flex;
-          align-items: center;
-          justify-content: flex-start;
-
-          .cctv-wrapper {
-            width: 40px;
-            height: 40px;
-            // margin: 0 auto;
-            background-image: url('/3d/images/label/cctv.svg#svgView(preserveAspectRatio(none))');
-            background-repeat: no-repeat;
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: 100% 100%;
-            pointer-events: all;
-            cursor: pointer;
-          }
-          
         }
       }
     }
