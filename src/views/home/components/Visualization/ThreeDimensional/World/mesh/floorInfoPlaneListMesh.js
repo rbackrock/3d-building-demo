1056c2c87cc148db124764589607cf242b56c12c
rbackrock <cokekokotea@gmail.com>
2023-02-27 23:09:00 +0800

    # update

--- src/views/home/components/Visualization/ThreeDimensional/World/mesh/floorInfoPlaneListMesh.js
+++
@@ -1,9 +0,0 @@
-import {  CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js'
-
-export default function() {
-  const css3Object = new CSS3DObject(document.querySelector('#floor-info-container-list-hook'))
-  css3Object.scale.set(0.3, 0.3, 0.3)
-  css3Object.position.set(-74, 0, 0)
-
-  return css3Object
-}
