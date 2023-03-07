fad2de9980fb2d741df2cf849dbe3ef4c2d932d3
yhuan <cokekokotea@gmail.com>
2023-03-07 10:23:41 +0800

    # update

--- src/views/home/components/Visualization/ThreeDimensional/resources/sources.js
+++ src/views/home/components/Visualization/ThreeDimensional/resources/sources.js
@@ -11,27 +11,27 @@ export default [
     name: sources.environmentMapTexture,
     type: sourceType.CUBE_TEXTURE,
     path:[
-      '/3d/images/sky-box/daytime/posx.jpg',
-      '/3d/images/sky-box/daytime/negx.jpg',
-      '/3d/images/sky-box/daytime/posy.jpg',
-      '/3d/images/sky-box/daytime/negy.jpg',
-      '/3d/images/sky-box/daytime/posz.jpg',
-      '/3d/images/sky-box/daytime/negz.jpg',
+      // '/3d/images/sky-box/daytime/posx.jpg',
+      // '/3d/images/sky-box/daytime/negx.jpg',
+      // '/3d/images/sky-box/daytime/posy.jpg',
+      // '/3d/images/sky-box/daytime/negy.jpg',
+      // '/3d/images/sky-box/daytime/posz.jpg',
+      // '/3d/images/sky-box/daytime/negz.jpg',
 
-      // 'http://cdn.rback.fun/building-demo/posx.jpg',
-      // 'http://cdn.rback.fun/building-demo/negx.jpg',
-      // 'http://cdn.rback.fun/building-demo/posy.jpg',
-      // 'http://cdn.rback.fun/building-demo/negy.jpg',
-      // 'http://cdn.rback.fun/building-demo/posz.jpg',
-      // 'http://cdn.rback.fun/building-demo/negz.jpg',
+      'http://cdn.rback.fun/building-demo/posx.jpg',
+      'http://cdn.rback.fun/building-demo/negx.jpg',
+      'http://cdn.rback.fun/building-demo/posy.jpg',
+      'http://cdn.rback.fun/building-demo/negy.jpg',
+      'http://cdn.rback.fun/building-demo/posz.jpg',
+      'http://cdn.rback.fun/building-demo/negz.jpg',
     ]
   },
   {
     name: sources.machineGltf,
     type: sourceType.GLTFMODEL,
     path: [
-      '/3d/models/machine.glb'
-      // 'http://cdn.rback.fun/building-demo/machine.glb'
+      // '/3d/models/machine.glb'
+      'http://cdn.rback.fun/building-demo/machine.glb'
     ]
   },
   {
@@ -40,8 +40,8 @@ export default [
     path: [
       // '/3d/models/building.glb'
       // '/3d/models/build0004-yasuo-quan.glb'
-      // 'http://cdn.rback.fun/building-demo/building.glb'
-      '/3d/models/building.glb'
+      'http://cdn.rback.fun/building-demo/building.glb'
+      // '/3d/models/building.glb'
     ]
   },
   // {
